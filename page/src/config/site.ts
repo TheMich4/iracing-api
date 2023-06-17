d0e672af35082dfeb9df605efae9023f47f7f577
Michal Dyczkowski <themich4@gmail.com>
2023-06-17 14:58:08 +0200

    Added next page

---
+++ page/src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "iracing-api",
+  description: "Documentation for iracing-api",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/themich4",
+    github: "https://github.com/themich4/iracing-api",
+    docs: "https://ui.shadcn.com",
+  },
+};
--- src/client.ts
+++ src/client.ts
@@ -9,21 +9,21 @@ export type CrateAxiosConfig = Omit<CreateAxiosDefaults, "baseURL">;
 export const createJar = () => new CookieJar();
 
 export const createAxiosInstance = (
-	jar: CookieJar,
-	config?: CrateAxiosConfig,
+  jar: CookieJar,
+  config?: CrateAxiosConfig,
 ) =>
-	wrapper(
-		axios.create({
-			...config,
-			baseURL: API_URL,
-			timeout: config?.timeout ?? 5000,
-			headers: {
-				"X-Requested-With": "XMLHttpRequest",
-				"Content-Type": "application/json",
-			},
-			jar,
-			withCredentials: true,
-		}),
-	);
+  wrapper(
+    axios.create({
+      ...config,
+      baseURL: API_URL,
+      timeout: config?.timeout ?? 5000,
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "application/json",
+      },
+      jar,
+      withCredentials: true,
+    }),
+  );
 
 export const axiosInstance = createAxiosInstance(createJar());
