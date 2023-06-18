0b2a617ed1e338cff9653695909dd6f551795bb9
Michal Dyczkowski <themich4@gmail.com>
2023-06-18 20:50:06 +0200

    Added links to method preview pages

--- page/src/app/endpoints/page.tsx
+++ page/src/app/endpoints/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -6,6 +7,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { endpointToMethod } from "@/config/endpoint-to-method";
+import { X } from "lucide-react";
+import Link from "next/link";
 
 import { getEndpoints } from "../api/get-enpoints";
 
@@ -21,17 +25,35 @@ const EndpointsPage = async () => {
           <TableRow>
             <TableHead>Service</TableHead>
             <TableHead>Endpoint</TableHead>
+            <TableHead>Method</TableHead>
           </TableRow>
         </TableHeader>
 
         <TableBody>
           {Object.entries(endpoints).map(([service, serviceEndpoints]) => (
-            Object.entries(serviceEndpoints).map(([endpoint, info]) => (
-              <TableRow>
-                <TableCell className="row-span-2">{service}</TableCell>
-                <TableCell>{`${service}/${endpoint}`}</TableCell>
-              </TableRow>
-            ))
+            Object.entries(serviceEndpoints).map(([endpoint, info]) => {
+              const method = endpointToMethod[`${service}/${endpoint}`];
+
+              return (
+                <TableRow>
+                  <TableCell className="row-span-2">{service}</TableCell>
+                  <TableCell>{`${service}/${endpoint}`}</TableCell>
+                  <TableCell>
+                    {method
+                      ? (
+                        <Link
+                          href={`/preview/${method}`}
+                          prefetch={false}
+                          className="hover:underline"
+                        >
+                          {method}
+                        </Link>
+                      )
+                      : <X className="w-5 h-5 text-red-500" />}
+                  </TableCell>
+                </TableRow>
+              );
+            })
           ))}
         </TableBody>
       </Table>
